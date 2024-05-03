960dfc9e0b2eaf7364982c9bbd2e5ea71308f5f8
Paidraig (Patrick) O'Ceallaigh <54907016+oceallaigh-p@users.noreply.github.com>
2024-05-03 16:05:12 -0500

    Update 03_transform_data.R

--- R/03_transform_data.R
+++ R/03_transform_data.R
@@ -1,5 +1,6 @@
 # Required libraries
 library(tidyverse)
+library(geosphere)
 
 # Required scripts
 
@@ -32,4 +33,33 @@ data_processed <- data_processed %>%
   mutate(
     rideable_type = as.factor(rideable_type),
     member_casual = as.factor(member_casual)
+  )
+
+## Rename variables ------------------------------------------------------------
+data_processed <- data_processed %>%
+  rename(
+    bike_type = rideable_type,
+    rider_type = member_casual,
+    start_timestamp = started_at,
+    end_timestamp = ended_at,
+    start_latitude = start_lat,
+    start_longitude = start_lng,
+    end_latitude = end_lat,
+    end_longitude = end_lng
+  )
+
+## Create new variables --------------------------------------------------------
+data_processed <- data_processed %>%
+  mutate(
+    ride_duration = as.numeric(difftime(end_timestamp, start_timestamp, units = "mins")),
+    ride_month = factor(floor_date(start_timestamp, "month")),
+    ride_week = factor(floor_date(start_timestamp, "week")),
+    ride_day_of_week = factor(wday(start_timestamp, label = TRUE)),
+    ride_start_hour = factor(hour(start_timestamp))
+  ) %>%
+  mutate(
+    ride_distance = distHaversine(
+      cbind(start_longitude, start_latitude),
+      cbind(end_longitude, end_latitude)
+    )
   )
