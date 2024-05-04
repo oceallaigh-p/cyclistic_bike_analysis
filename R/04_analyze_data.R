58ba6826728494812fd451dd4816129783a62951
Paidraig (Patrick) O'Ceallaigh <54907016+oceallaigh-p@users.noreply.github.com>
2024-05-04 09:51:28 -0500

    Update 04_analyze_data.R

--- R/04_analyze_data.R
+++ R/04_analyze_data.R
@@ -1,10 +1,32 @@
 # Required libraries
 library(tidyverse)
+library(cowplot) 
+
+# Collect the lazy data table to a tibble ---------------------------------------
+data_processed <- data_processed %>%
+  collect()
 
 # Examine monthly ridership data ------------------------------------------------
+data_processed %>%
+  group_by(ride_month,
+           rider_type) %>%
+  summarise(count = n()) %>%
+  ungroup() %>%
+  ggplot(aes(x = ride_month, 
+             y = count, 
+             fill = rider_type)) +
+  geom_col(position = "dodge") +
+  theme_minimal_grid() +
+  labs(title = "Monthly Ridership by Rider Type",
+       x = "Month",
+       y = "Ride Count",
+       fill = "Rider Type")
+  
 
 # Examine weekly ridership data -------------------------------------------------
 
 # Examine daily ridership data --------------------------------------------------
 
 # Examine hourly ridership data -------------------------------------------------
+
+# Examine bike type data --------------------------------------------------------
