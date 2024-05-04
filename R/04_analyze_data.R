c67e3a5ddb89e42dbf7d86fc8140a93e743ea7db
Paidraig (Patrick) O'Ceallaigh <54907016+oceallaigh-p@users.noreply.github.com>
2024-05-04 09:29:48 -0500

    Create 04_analyze_data.R

---
+++ R/04_analyze_data.R
@@ -0,0 +1,10 @@
+# Required libraries
+library(tidyverse)
+
+# Examine monthly ridership data ------------------------------------------------
+
+# Examine weekly ridership data -------------------------------------------------
+
+# Examine daily ridership data --------------------------------------------------
+
+# Examine hourly ridership data -------------------------------------------------
