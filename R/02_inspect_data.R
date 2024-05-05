71f2691dbc91de850a7857962ceb13adc8a3b304
Paidraig (Patrick) O'Ceallaigh <54907016+oceallaigh-p@users.noreply.github.com>
2024-05-05 16:19:40 -0500

    Update 02_inspect_data.R

--- R/02_inspect_data.R
+++ R/02_inspect_data.R
@@ -6,9 +6,9 @@ library(here)
 source(here("R", "nrow_lazy_dt.R"))
 source(here("R", "count_categorical.R"))
 
-# Data Inspection --------------------------------------------------------------
-
-## Check structure and type of data --------------------------------------------
+# ==============================================================================
+# Check structure and type of data
+# ==============================================================================
 
 data_raw %>%
   glimpse()
@@ -16,35 +16,37 @@ data_raw %>%
 obs_raw <- data_raw %>%
   nrow_lazy_dt()
 
-## Check duplicate observations ------------------------------------------------
-
+# ==============================================================================
+# Check duplicate observations
+# ==============================================================================
 duplicate_obs <- data_raw %>%
   group_by_all() %>%
   filter(n() > 1) %>%
   ungroup() %>%
   nrow_lazy_dt()
-  
-## Check missing values ---------------------------------------------------------
+
+# ==============================================================================
+# Check missing values
+# ==============================================================================
 
 missing_obs <- data_raw %>%
-    summarise(
-      across(everything(), ~ sum(is.na(.), na.rm = TRUE))
-    ) %>%
-    pivot_longer(
-      everything(),
-      names_to = "variable",
-      values_to = "missing_count"
-    ) %>%
-    filter(missing_count > 0) %>%
-    as_tibble()
+  summarise(across(everything(), ~ sum(is.na(.), na.rm = TRUE))) %>%
+  pivot_longer(
+    everything(),
+    names_to = "variable",
+    values_to = "missing_count"
+  ) %>%
+  filter(missing_count > 0) %>%
+  as_tibble()
 
-## Check categorical variables -------------------------------------------------
+# ==============================================================================
+# Check categorical variables
+# ==============================================================================
 
-# Check for valid values in the rideable_type variable
+# Check for valid values in the rideable_type variable -------------------------
 count_rideable_type <- data_raw %>%
   count_categorical(rideable_type)
 
-# Check for valid values in the member_casual variable
+# Check for valid values in the member_casual variable -------------------------
 count_member_casual <- data_raw %>%
   count_categorical(member_casual)
-
