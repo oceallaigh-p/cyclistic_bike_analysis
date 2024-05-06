eb99e76df27e2ef87a00c4f82a26ef3101f19911
Paidraig (Patrick) O'Ceallaigh <54907016+oceallaigh-p@users.noreply.github.com>
2024-05-06 08:33:38 -0500

    Create render_markdown.R

---
+++ R/render_markdown.R
@@ -0,0 +1,11 @@
+library(here)
+
+# Path variables for knitted html
+input_file <- here("reports", "cyclistic_bikeshare_report.Rmd")
+output_dir <- here("output", "html")
+output_file <- "index.html"
+
+# Render the Rmd file to the specified directory
+rmarkdown::render(input_file,
+                  output_dir = output_dir,
+                  output_file = output_file)
