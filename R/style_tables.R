34447b6f11aab76bffdd7b62a554a19cdc17e96f
Paidraig (Patrick) O'Ceallaigh <54907016+oceallaigh-p@users.noreply.github.com>
2024-05-27 08:49:59 -0500

    Update style_tables.R

--- R/style_tables.R
+++ R/style_tables.R
@@ -1,42 +1,43 @@
 # Required libraries
 library(kableExtra)
 
-#' Apply Custom Style to Tables
+#' Custom Table Theme
 #'
-#' This function applies a custom style to tables created with `knitr::kable`.
-#' It uses the `kable_minimal` style from `kableExtra` with additional bootstrap
-#' options to enhance the appearance of the table. The function is designed to
-#' make tables more visually appealing with options like hover, condensed view,
-#' and responsiveness.
+#' This function applies styling to a kable input based on the output format.
 #'
-#' @param kable_input A kable object created by `knitr::kable` that you want
-#'        to style.
+#' @param kable_input A kable object to be styled.
+#' @return A styled kable object.
 #'
-#' @return A kable object styled with minimal theme options and additional
-#'         bootstrap features. The table is not set to full width and is centered.
-#'
-#' @details The function utilizes `kableExtra::kable_minimal` to apply a
-#'          minimalist theme to the kable output. Bootstrap options such as
-#'          'hover', 'condensed', and 'responsive' are used to enhance table
-#'          interactivity and fit. The table is configured to not use the full
-#'          width of the page and is centered for better visual alignment in
-#'          reports or web pages.
+#' @details
+#' This function checks the output format (HTML or LaTeX) and applies
+#' appropriate styling using the `kable_styling` function from the
+#' `kableExtra` package.
 #'
 #' @examples
+#' # Example usage:
 #' library(knitr)
 #' library(kableExtra)
-#' data(mtcars)
-#' kable_output <- kable(mtcars, format = "html")
-#' styled_table <- custom_table_theme(kable_output)
-#' print(styled_table)
+#' library(dplyr)
+#' kable_input <- knitr::kable(head(mtcars), "html")
+#' custom_table_theme(kable_input)
 #'
-#' @importFrom kableExtra kable_minimal
-
+#' @import knitr
+#' @import dplyr
+#' @import kableExtra
 custom_table_theme <- function(kable_input) {
-  kable_input %>%
-    kable_styling(
-      bootstrap_options = c("hover", "condensed", "responsive"),
-      full_width = FALSE,
-      position = "center"
-    )
+  if (format == "html") {
+    kable_input %>%
+      kable_styling(
+        bootstrap_options = c("hover", "condensed", "responsive"),
+        full_width = FALSE,
+        position = "center"
+      )
+  } else {
+    kable_input %>%
+      kable_styling(
+        latex_options = "striped",
+        full_width = FALSE,
+        position = "center"
+      )
+  }
 }
