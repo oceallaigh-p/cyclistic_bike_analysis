229bf6303c09aec92073eb9f531dbfdb5571166e
Paidraig (Patrick) O'Ceallaigh <54907016+oceallaigh-p@users.noreply.github.com>
2024-05-29 10:34:11 -0500

    Update find_mode.R

--- R/find_mode.R
+++ R/find_mode.R
@@ -1,25 +1,30 @@
 # Required libraries
 library(dplyr)
 
-#' Find the Mode of a Column in a Tibble
+#' Find Mode
 #'
-#' This function calculates the mode(s) of a specified column in a tibble. The mode is defined
-#' as the value(s) that appear most frequently in the column. If there are multiple modes,
-#' all of them will be returned.
+#' Identify the mode(s) of a specified variable in a data frame.
 #'
-#' @param data A tibble containing the data to analyze.
-#' @param variable The name of the column (unquoted) whose mode is to be calculated.
+#' @param data A data frame, tibble, or `dtplyr::lazy_dt` containing the data.
+#' @param variable The variable within the data for which to find the mode.
+#'   This should be an unquoted variable name.
 #'
-#' @return A vector containing the mode(s) of the specified column.
+#' @return The mode(s) of the specified variable.
 #'
-#' @details The function uses `dplyr` for data manipulation, counting each unique value in the specified
-#' column and then determining which value(s) appear most frequently. The use of `{{ variable }}` ensures
-#' tidy evaluation of the column name.
+#' @details This function calculates the mode(s) of a given variable by counting
+#'   the frequency of each value and identifying the value(s) with the highest
+#'   frequency.
 #'
 #' @examples
-#' # Assuming a tibble `df` with a column `age`
-#' df <- tibble(age = c(25, 30, 25, 40, 45, 30, 30, 25, 45))
-#' find_mode(df, age)
+#' \dontrun{
+#' library(dplyr)
+#'
+#' data <- data.frame(
+#'   values = c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
+#' )
+#' mode_value <- find_mode(data, values)
+#' print(mode_value)
+#' }
 #'
 #' @importFrom dplyr count
 #' @importFrom dplyr filter
